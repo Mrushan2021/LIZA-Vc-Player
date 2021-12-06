9762a542f8463a9653196bdc235b2f00ae2079ff
MR USHAN <89759475+Mrushan2021@users.noreply.github.com>
2021-12-06 08:12:06 +0530

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -15,6 +15,6 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://music:music@cluster0.sh6h4.
 SUDO_USERS = list(map(int, getenv('SUDO_USERS', '1669178360').split()))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ''))
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "SUPERIOR_BOTS")
-ZAID_SUPPORT = getenv("ZAID_SUPPORT", "SUPERIOR_SUPPORT")
+ZAID_SUPPORT = getenv("LIZA_SUPPORT", "SUPERIOR_SUPPORT")
 ASS_ID = int(getenv("ASS_ID", ''))
 OWNER_ID = list(map(int, getenv('OWNER_ID', '').split()))
